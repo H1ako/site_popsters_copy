ded971023abd5bfb9b867f67451555df7f249346
H1ako <nikita-sobolev-wd@yandex.ru>
2021-07-21 19:14:15 +0400

    add project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Popsters</title>
+    <link rel="stylesheet" href="/css/index.css">
+</head>
+<body>
+    <header>
+        <div class="section-inner">
+            <div class="top-bar">
+                <div class="top-bar__logo">
+                    <img src="/Images/logohdpi.svg" alt="Popsters">
+                </div>
+                <nav>
+                    <ul>
+                        <li>MAIN</li>
+                        <li>FAQ</li>
+                        <li>PRICING</li>
+                        <li>BLOG</li>
+                    </ul>
+                </nav>
+                <select class="top-bar__lang" name="language">
+                    <option value="RU" selected>RU</option>
+                    <option value="EN">ENG</option>
+                </select>
+            </div>
+            <main>
+                <div class="info">
+                    <div class="info__slogan">
+                        Get social activity insights to make your marketing better than ever
+                    </div>
+                    <div class="info__dscrp">
+                        Popsters helps to compare, to get stats and measure efficiency of posts of any social page that you are interested in
+                    </div>
+                    <button>TRY FOR FREE</button>
+                </div>
+                <div class="img">
+                    <img src="/Images/arts/main.svg" alt="">
+                </div>
+            </main>
+        </div>
+    </header>
+    <script type="text/javascript" src="/js/jquery.min.js"></script>
+</body>
+</html>
